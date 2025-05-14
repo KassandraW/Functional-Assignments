955fbb78978bab159feda15790c08ec76da3c582
kassandra <kwad@itu.dk>
2025-05-14 12:06:44 +0200

    ee

--- exam24/obj/Debug/net7.0/FPReExam2024.AssemblyInfo.fs
+++ exam24/obj/Debug/net7.0/FPReExam2024.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("FPReExam2024")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fb368896c0e0dae0146cad0cff4878efab02476")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3656de255f96870a43cfab3d55b901f0fdbc1424")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("FPReExam2024")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("FPReExam2024")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
