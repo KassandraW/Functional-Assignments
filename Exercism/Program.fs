34c8b033d37780e25485c0e1c83201f595aa041c
kassandra <kwad@itu.dk>
2025-05-17 16:50:50 +0200

    RNA protein exercise

--- Exercism/Program.fs
+++ Exercism/Program.fs
@@ -1,1 +1,28 @@
-﻿
+﻿module ExercismExercices
+
+
+let mapRNA s =
+        match s with
+        | "AUG" -> "Methionine"
+        | "UUU" | "UUC" -> "Phenylalanine"
+        | "UUA" | "UUG" -> "Leucine"
+        | "UCU" | "UCC" | "UCA" |"UCG" -> "Serine"
+        | "UAU" | "UAC" -> "Tyrosine"
+        | "UGU" | "UGC" -> "Cysteine"
+        | "UGG" -> "Tryptophan"
+        | "UAA" | "UAG" | "UGA" -> "Stop"
+        | _ -> ""
+        
+let proteins (rna: string) =
+    let rec aux (rn : string) acc =
+        match rn with
+        | "" -> acc
+        | _ -> 
+            let sub = mapRNA rn[0..2]
+            match sub with
+            | "Stop" -> acc
+            | _ -> 
+                let rest = rn[ 3 .. ]
+                aux rest (sub :: acc) 
+    (aux rna []) |> List.rev  
+    
